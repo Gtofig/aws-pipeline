06492d9197ca46e0c644e910c1be0c7b4b4f889c
Tofig Hasanov <gtofig@yahoo.com>
2021-05-02 10:01:30 +0100

    Add source action to the pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,9 +1,31 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Artifact, Pipeline } from "@aws-cdk/aws-codepipeline";
+import { GitHubSourceAction } from "@aws-cdk/aws-codepipeline-actions";
+import { SecretValue } from "@aws-cdk/core";
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new Pipeline(this, "Pipeline", {
+      pipelineName: "Pipeline",
+      crossAccountKeys: false,
+    });
+
+    const sourceOutput = new Artifact("SourceOutput");
+
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new GitHubSourceAction({
+          owner: "Gtofig",
+          repo: "aws-pipeline",
+          branch: "master",
+          actionName: "Pipeline Source",
+          oauthToken: SecretValue.secretsManager("github-token"),
+          output: sourceOutput,
+        }),
+      ],
+    });
   }
 }

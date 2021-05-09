193cf2c77933e38e765eb1ba8a39389bb594167b
Tofig Hasanov <gtofig@yahoo.com>
2021-05-09 11:17:32 +0100

    Add Pipeline Update stage

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import { Artifact, Pipeline } from "@aws-cdk/aws-codepipeline";
 import {
+  CloudFormationCreateUpdateStackAction,
   CodeBuildAction,
   GitHubSourceAction,
 } from "@aws-cdk/aws-codepipeline-actions";
@@ -53,6 +54,18 @@ export class PipelineStack extends cdk.Stack {
               "build-specs/cdk-build-spec.yml"
             ),
           }),
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: "Pipeline_Update",
+      actions: [
+        new CloudFormationCreateUpdateStackAction({
+          actionName: "Pipeline_Update",
+          stackName: "PipelineStack",
+          templatePath: cdkBuildOutput.atPath("PipelineStack.template.json"),
+          adminPermissions: true,
         }),
       ],
     });

8055b497a23e53f0b784ecc3a2d23b1a1c32aa5a
Tofig Hasanov <gtofig@yahoo.com>
2021-05-09 16:10:12 +0100

    Fix pipeline unit test

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,13 +1,17 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Pipeline from '../lib/pipeline-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  SynthUtils,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Pipeline from "../lib/pipeline-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Pipeline Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Pipeline.PipelineStack(app, "MyTestStack");
+  // THEN
+
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });

cc67e1ccb3ea0cc2d73e0e7fee6a8cbdee84f897
Tofig Hasanov <gtofig@yahoo.com>
2021-05-09 11:18:27 +0100

    Revert "Add Pipeline Update stage"
    This reverts commit 193cf2c77933e38e765eb1ba8a39389bb594167b.

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import { Artifact, Pipeline } from "@aws-cdk/aws-codepipeline";
 import {
-  CloudFormationCreateUpdateStackAction,
   CodeBuildAction,
   GitHubSourceAction,
 } from "@aws-cdk/aws-codepipeline-actions";
@@ -54,18 +53,6 @@ export class PipelineStack extends cdk.Stack {
               "build-specs/cdk-build-spec.yml"
             ),
           }),
-        }),
-      ],
-    });
-
-    pipeline.addStage({
-      stageName: "Pipeline_Update",
-      actions: [
-        new CloudFormationCreateUpdateStackAction({
-          actionName: "Pipeline_Update",
-          stackName: "PipelineStack",
-          templatePath: cdkBuildOutput.atPath("PipelineStack.template.json"),
-          adminPermissions: true,
         }),
       ],
     });

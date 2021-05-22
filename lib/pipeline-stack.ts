f93c43ca0c744f470e04301b3ce7eb84c94e6415
Tofig Hasanov <gtofig@yahoo.com>
2021-05-22 20:07:16 +0100

    Restart pipeline on update

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -19,6 +19,7 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new Pipeline(this, "Pipeline", {
       pipelineName: "Pipeline",
       crossAccountKeys: false,
+      restartExecutionOnUpdate: true,
     });
 
     const cdkSourceOutput = new Artifact("CDKSourceOutput");

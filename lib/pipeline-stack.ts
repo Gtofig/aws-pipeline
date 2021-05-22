10d8c489dc830dbc7dc12b9cb49f9f2201329d2d
Tofig Hasanov <gtofig@yahoo.com>
2021-05-22 19:20:39 +0100

    Add service source action to the pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -21,7 +21,8 @@ export class PipelineStack extends cdk.Stack {
       crossAccountKeys: false,
     });
 
-    const sourceOutput = new Artifact("SourceOutput");
+    const cdkSourceOutput = new Artifact("CDKSourceOutput");
+    const serviceSourceOutput = new Artifact("ServiceSourceOutput");
 
     pipeline.addStage({
       stageName: "Source",
@@ -32,7 +33,15 @@ export class PipelineStack extends cdk.Stack {
           branch: "master",
           actionName: "Pipeline_Source",
           oauthToken: SecretValue.secretsManager("github-token"),
-          output: sourceOutput,
+          output: cdkSourceOutput,
+        }),
+        new GitHubSourceAction({
+          owner: "Gtofig",
+          repo: "express-lambda",
+          branch: "master",
+          actionName: "Service_Source",
+          oauthToken: SecretValue.secretsManager("github-token"),
+          output: serviceSourceOutput,
         }),
       ],
     });
@@ -44,7 +53,7 @@ export class PipelineStack extends cdk.Stack {
       actions: [
         new CodeBuildAction({
           actionName: "CDK_Build",
-          input: sourceOutput,
+          input: cdkSourceOutput,
           outputs: [cdkBuildOutput],
           project: new PipelineProject(this, "CdkBuildProject", {
             environment: {
